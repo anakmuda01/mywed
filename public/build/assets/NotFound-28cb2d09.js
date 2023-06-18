23173f9a1bd995487aacb082da50906d74c99347
anakmuda01 <fhmsholihin09@gmail.com>
2023-06-18 15:18:48 +0800

    eeee

--- public/build/assets/NotFound-28cb2d09.js
+++
@@ -1,1 +0,0 @@
-import{a5 as e,e as c,p as n}from"./app-a1bbad2b.js";const o={};function t(r,a){return c(),n("div",null,"NOT FFOUND")}const _=e(o,[["render",t]]);export{_ as default};
--- resources/js/components/landing/MyWed.vue
+++ resources/js/components/landing/MyWed.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="tangah">
-    <div class="cantik-text text-center text-h5 text-deep-orange-10">Sholihin & Inayati</div>
+    <div class="cantik-text text-center text-h5 text-deep-orange-10">
+      Sholihin & Inayati
+    </div>
     <div>
       <q-img alt="corefront" :src="corefront" width="250px"></q-img>
     </div>
     <div class="text-h6 text-center">
       <div class="cantik-text">Our <span class="text-red">❤</span> Story</div>
-      <div class="cantik-text">Begins Here</div>
+      <div class="cantik-text" @click="router.push('/app/inayyy-ehem')">
+        Begins Here
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
 import corefront from "@/assets/corefrontSmall.png";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
 </script>
 
 <style>
