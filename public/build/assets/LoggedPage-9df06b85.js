97a07b695c24747e78071596d6a87ffae3f23d76
anakmuda01 <fhmsholihin09@gmail.com>
2023-06-18 14:48:15 +0800

    public build

---
+++ public/build/assets/LoggedPage-9df06b85.js
@@ -0,0 +1,1 @@
+import{a5 as e,e as c,p as n}from"./app-a1bbad2b.js";const o={};function r(t,a){return c(),n("div",null,"LOGGED PAGE")}const _=e(o,[["render",r]]);export{_ as default};
---
+++ public/build/assets/Login-3de18def.js
@@ -0,0 +1,1 @@
+import{a5 as e,e as c,p as n}from"./app-a1bbad2b.js";const t={};function o(r,s){return c(),n("div",null,"tes")}const _=e(t,[["render",o]]);export{_ as default};
